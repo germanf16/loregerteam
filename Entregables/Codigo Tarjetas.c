a9b2f689d09797474b59f898296f0d10995fd0fc
lorena hernandez <lpaulette.hernandez@edu.uag.mx>
2020-10-22 08:45:47 -0500

    cambios

--- Entregables/Codigo Tarjetas.c
+++ Entregables/Codigo Tarjetas.c
@@ -2,11 +2,26 @@
 #include "stdio.h"
 #include "codigotarj.h"
 
+
 void main (void)
 
 {
+	uint8 au8CardNumber[16]= {5,2,3,1,7,9,5,3,8,1,5,3,1,8,9,2};
+	uint8 u8Check = 0;
+	u8Check = u8LuhnCheck (&au8CardNumber[0]);
 
+	if ( u8Check == 0 )
+	{
+
+	}
+	else
+	{
+
+	}	
+
+}
+
+uint8 u8LuhnCheck ( uint8 *pu8Data )
+{
 	
-
-
 }
