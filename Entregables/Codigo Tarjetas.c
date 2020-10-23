8a8733264874e8b6f1141f7da1c2bc5f848d5fee
lorena hernandez <lpaulette.hernandez@edu.uag.mx>
2020-10-22 23:53:07 -0500

    CHANGUES

--- Entregables/Codigo Tarjetas.c
+++ Entregables/Codigo Tarjetas.c
@@ -6,22 +6,26 @@
 void main (void)
 
 {
+	
 	uint8 au8CardNumber[16]= {5,2,3,1,7,9,5,3,8,1,5,3,1,8,9,2};
 	uint8 u8Check = 0;
+
 	u8Check = u8LuhnCheck (&au8CardNumber[0]);
+
+	printf("NUMERO DE TARJETA");
 
 	if ( u8Check == 0 )
 	{
-
+		printf("\n NUMERO DE TARJETA VALIDO ",)	
 	}
 	else
 	{
 
 	}	
-
+	
 }
 
 uint8 u8LuhnCheck ( uint8 *pu8Data )
 {
-	
+
 }
